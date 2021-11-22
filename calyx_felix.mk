3e3d3c1ab3c3fcb11b59d3f63c2f7d6347897600
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Calyx product makefiles
    Co-authored-by: Michael Bestas <mikeioannina@gmail.com>
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ calyx_felix.mk
@@ -0,0 +1,31 @@
+#
+# SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/calyx/config/common_foldable_book_telephony.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := felix
+DEVICE_PATH := device/google/felix
+$(call inherit-product, device/google/gs201/calyx_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-calyx.mk)
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel Fold
+PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
+
+PRODUCT_SYSTEM_NAME := $(DEVICE_CODENAME)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
+    PRIVATE_BUILD_DESC="felix-user 15 AP3A.241005.015 12366759 release-keys"
+
+BUILD_FINGERPRINT := google/felix/felix:15/AP3A.241005.015/12366759:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
