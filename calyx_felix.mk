d1a2298b72e360541b8575194cc062be159ea262
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Calyx product makefiles
    Change-Id: I93e489c0e5fb7f8282b25a2b59c8e2e64a4b86b7

---
+++ calyx_felix.mk
@@ -0,0 +1,20 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/gs201/calyx_common.mk)
+$(call inherit-product, device/google/felix/device-calyx.mk)
+$(call inherit-product, device/google/felix/aosp_felix.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_felix
+PRODUCT_MODEL := Pixel Fold
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=felix \
+    PRIVATE_BUILD_DESC="felix-user 13 TQ3C.230605.010.C1 10167124 release-keys"
+
+BUILD_FINGERPRINT := google/felix/felix:13/TQ3C.230605.010.C1/10167124:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
