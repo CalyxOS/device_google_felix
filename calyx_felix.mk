6f6b2d7bedf21cdae27cb7919ff3762ceecf0a11
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    felix: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- calyx_felix.mk
+++ calyx_felix.mk
@@ -11,6 +11,7 @@ $(call inherit-product, vendor/calyx/config/common_foldable_book_telephony.mk)
 # Inherit device configuration
 DEVICE_CODENAME := felix
 DEVICE_PATH := device/google/felix
+VENDOR_PATH := vendor/google/felix
 $(call inherit-product, device/google/gs201/calyx_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-calyx.mk)
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
@@ -29,3 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/felix/felix:14/AP2A.240905.003/12231197:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
