e2ad0a2edea20b40fcb29fbae4ab198befbf4947
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-12 04:11:07 +0530

    Build EuiccSupportPixelOverlay
    Change-Id: I46db4122b89798457338f6751982865571529ba6

--- device-calyx.mk
+++ device-calyx.mk
@@ -5,6 +5,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/felix/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/felix/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
