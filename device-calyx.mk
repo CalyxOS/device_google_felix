c6b0ab8c8962797fb8a1e66f1f15520290fbb0aa
Michael Bestas <mkbestas@gmail.com>
2023-07-16 15:58:40 +0300

    felix: overlay: Import selected framework-res/Settings product RRO overlays
    Change-Id: I375809d68a093be9b0a17d761bda0b41e1e4cac4

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,8 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/felix/overlay-calyx
 
+PRODUCT_PACKAGES += \
+    SettingsFoldAutoRotateOverlay
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
