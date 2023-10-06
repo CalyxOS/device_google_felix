92f073b641e62921ab6443c075a9c04f97948fbf
Michael Bestas <mkbestas@gmail.com>
2023-10-06 20:03:59 +0300

    Include wireless_charger makefile
    This includes the required sepolicy and compatibility matrix
    in the build.
    
    Change-Id: I3096357c5f45e664a7b39870fdc053e48d1361ad

--- device-calyx.mk
+++ device-calyx.mk
@@ -27,6 +27,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/permissions_com.google.android.apps.camera.services.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.apps.camera.services.xml
 
+# wireless_charger HAL service
+include device/google/gs-common/wireless_charger/wireless_charger.mk
+
 # Build necessary packages for vendor
 
 # Codec2
