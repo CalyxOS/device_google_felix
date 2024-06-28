400d020fc6c6a066a9ab347d86c980d46edd0294
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-28 06:40:09 +0530

    Enable freeform window management
    Issue: calyxos#2090
    Change-Id: I49f72b6f4da5ca6a05e3c218b80a950c55bec2ee

--- device-calyx.mk
+++ device-calyx.mk
@@ -16,6 +16,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/google/felix/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
+# Freeform window management
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
 # HBM
 PRODUCT_COPY_FILES += \
     device/google/felix/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
