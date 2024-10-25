fe8aad9bdf5e3e4c27f5a59348144ff66aadad06
Michael Bestas <mkbestas@gmail.com>
2024-10-25 21:55:33 +0300

    Adapt to new build prop override scheme again
    Change-Id: I5f68f971e18aec5f695ecdaf4affa72eea3a5a97

--- calyx_felix.mk
+++ calyx_felix.mk
@@ -22,7 +22,7 @@ PRODUCT_MODEL := Pixel Fold
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc=felix-user[[:space:]]15[[:space:]]AP3A.241005.015[[:space:]]12366759[[:space:]]release-keys \
+    BuildDesc="felix-user 15 AP3A.241005.015 12366759 release-keys" \
     BuildFingerprint=google/felix/felix:15/AP3A.241005.015/12366759:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
