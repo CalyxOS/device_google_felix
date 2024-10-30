884f7052f0837bdc45e2054e8b390c99848ca44f
Michael Bestas <mkbestas@lineageos.org>
2024-10-30 08:35:13 +0200

    felix: Add felix-services.jar
    Change-Id: Ia18181006fbea8d09ff121a8dc94086b92dbcf9c

--- device-calyx.mk
+++ device-calyx.mk
@@ -18,6 +18,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# Fold extensions
+PRODUCT_SYSTEM_SERVER_JARS += \
+    system_ext:felix-services
+
 # For Google Camera
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/sysconfig/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
