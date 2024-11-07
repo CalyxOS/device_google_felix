50d264734379311eea880389c6457703459cf048
Michael Bestas <mikeioannina@gmail.com>
2024-11-07 15:00:41 +0200

    Update fingerprint/build description from AP3A.241105.007
    Change-Id: I9b25b08bae4aa061cc9e2caf87108c8e3f234cf4

--- calyx_felix.mk
+++ calyx_felix.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel Fold
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="felix-user 15 AP3A.241005.015 12366759 release-keys" \
-    BuildFingerprint=google/felix/felix:15/AP3A.241005.015/12366759:user/release-keys \
+    BuildDesc="felix-user 15 AP3A.241105.007 12470370 release-keys" \
+    BuildFingerprint=google/felix/felix:15/AP3A.241105.007/12470370:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
