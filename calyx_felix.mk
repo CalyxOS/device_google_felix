5cda4b84f3aea4f222a8eefd992418a350b894ed
Chirayu Desai <chirayudesai1@gmail.com>
2025-01-08 04:26:24 +0530

    Update fingerprint/build description from AP4A.250105.002
    Change-Id: I5cac60e5fff2adbca694ff706c42c0a85d14bcba

--- calyx_felix.mk
+++ calyx_felix.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel Fold
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="felix-user 15 AP4A.241205.013 12621605 release-keys" \
-    BuildFingerprint=google/felix/felix:15/AP4A.241205.013/12621605:user/release-keys \
+    BuildDesc="felix-user 15 AP4A.250105.002 12701944 release-keys" \
+    BuildFingerprint=google/felix/felix:15/AP4A.250105.002/12701944:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
