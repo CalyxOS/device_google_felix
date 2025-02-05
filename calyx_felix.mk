6da6206711f563b08da9ff2cfa485f4cdbf9fea2
Michael Bestas <mkbestas@gmail.com>
2025-02-05 05:15:56 +0200

    Update fingerprint/build description from AP4A.250205.002
    Change-Id: I2492ae42048adac9c1727cce8e8e1e15e4ce9f96

--- calyx_felix.mk
+++ calyx_felix.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel Fold
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="felix-user 15 AP4A.250105.002 12701944 release-keys" \
-    BuildFingerprint=google/felix/felix:15/AP4A.250105.002/12701944:user/release-keys \
+    BuildDesc="felix-user 15 AP4A.250205.002 12821496 release-keys" \
+    BuildFingerprint=google/felix/felix:15/AP4A.250205.002/12821496:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
