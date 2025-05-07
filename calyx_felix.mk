61d80cdf04cbe686fd7dbc357b86f750af22d6a7
Michael Bestas <mkbestas@gmail.com>
2025-05-07 04:45:24 +0300

    Update fingerprint/build description from BP1A.250505.005.B1
    Change-Id: If257666af7a12e7b1fb509713a3e829871053bb5

--- calyx_felix.mk
+++ calyx_felix.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel Fold
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="felix-user 15 BP1A.250405.007.B1 13240308 release-keys" \
-    BuildFingerprint=google/felix/felix:15/BP1A.250405.007.B1/13240308:user/release-keys \
+    BuildDesc="felix-user 15 BP1A.250505.005.B1 13277630 release-keys" \
+    BuildFingerprint=google/felix/felix:15/BP1A.250505.005.B1/13277630:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
